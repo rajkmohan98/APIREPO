deb1d91fc428496ad730214f4e908c74d6ca7145
rajkmohan98 <rajkmohan1998@gmail.com>
2024-04-28 13:13:46 +0530

    added views

--- user_data/views.py
+++ user_data/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
 # Create your views here.
+class NewAPIView(APIView):
+    def post(self,request):
+        pass
+    def get(self,request):
+        pass
+    def put(self,request):
+        pass
